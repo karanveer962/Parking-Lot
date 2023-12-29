388d335f834d31edfbcfb847f41d55b16de0dd43
karanveer962 <karanveer962@gmail.com>
2023-12-29 13:52:46 +0530

    Implement parking lot full sign feature:UC3

--- src/main/java/org/example/ParkingLot.java
+++ src/main/java/org/example/ParkingLot.java
@@ -1,13 +1,28 @@
 package org.example;
-
 public class ParkingLot {
+    public int MAX_CAPACITY = 10;
+    public int count=0;
     public boolean parkCarForFlight() {
-        System.out.println("Vehicle Parked ✅");
-        return true;
+        if(!isLotFull()) {
+            System.out.println("Vehicle Parked ✅");
+            count++;
+            return true;
+        }
+        else
+            return false;
     }
     public boolean unParkCar() {
-        System.out.println("Vehicle UnParked ✅");
-        return true;
+        if(count==0){
+            System.out.println("Error!!! No car in the Lot");
+            return false;
+        }
+        else {
+            System.out.println("Vehicle UnParked ✅");
+            count--;
+            return true;
+        }
     }
-
+    public boolean isLotFull() {
+        return count == MAX_CAPACITY;
+    }
 }
--- src/test/java/org/example/ParkingLotTest.java
+++ src/test/java/org/example/ParkingLotTest.java
@@ -1,19 +1,42 @@
 package org.example;
 
+import org.junit.Before;
 import org.junit.Test;
-import static junit.framework.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+
+import static junit.framework.Assert.*;
 
 public class ParkingLotTest {
-    @Test
-    public void parkCarForFlight_ShouldReturnTrue_ForEveryDriverJourney() {
-        ParkingLot parkingLot = new ParkingLot();
-        boolean isParked = parkingLot.parkCarForFlight();
-        assertTrue(isParked);
+    private ParkingLot parkingLot;
+    @Before
+    public void setUp() {
+        parkingLot = new ParkingLot();
     }
     @Test
-    public void unParkCar_ShouldReturnTrue_ForEveryVehicle() {
-        ParkingLot parkingLot = new ParkingLot();
+    public void parkCarForFlight_ShouldReturnFalse_WhenLotCapacityIsZero() {
+        parkingLot.MAX_CAPACITY=0;
+        boolean isParked = parkingLot.parkCarForFlight();
+        assertFalse(isParked);
+        assertEquals(0,parkingLot.count);
+    }
+    @Test
+    public void parkCarForFlight_ShouldReturnTrue_WhenLotCapacityIsMoreThanCount() {
+        boolean isParked = parkingLot.parkCarForFlight();
+        assertTrue(isParked);
+        assertEquals(1,parkingLot.count);
+    }
+    @Test
+    public void unParkCar_ShouldReturnTrue_WhenLotHasSomeCars() {
+        parkingLot.parkCarForFlight();
+        parkingLot.parkCarForFlight();
         boolean unParked = parkingLot.unParkCar();
         assertTrue(unParked);
+        assertEquals(1,parkingLot.count);
+    }
+    @Test
+    public void unParkCar_ShouldReturnFalse_WhenNoCarIsPresent() {
+        boolean unParked = parkingLot.unParkCar();
+        assertFalse(unParked);
+        assertEquals(0,parkingLot.count);
     }
 }
