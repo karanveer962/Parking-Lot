d8917dea5ba5f86cbc879fa45c45508c73a87da6
karanveer962 <karanveer962@gmail.com>
2023-12-29 11:10:36 +0530

    Introduce welcome message in the main branch

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Warm welcome to my Parking Lot Service!!!" );
+    }
+}
